9c925aa7c57a261fd7700a514704aaf3b0846634
Sneha <sneha.nalla@sjsu.edu>
2026-04-27 22:41:29 -0700

    added all tables to dump.sql

--- dump.sql
+++ dump.sql
@@ -24,6 +24,115 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `cs157a_library_system` /*!40100 DEFAUL
 USE `cs157a_library_system`;
 
 --
+-- Table structure for table `Books`
+--
+
+DROP TABLE IF EXISTS `Books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Books` (
+  `BookID` int unsigned NOT NULL AUTO_INCREMENT,
+  `ISBN` varchar(13) DEFAULT NULL,
+  `Status` varchar(100) NOT NULL,
+  PRIMARY KEY (`BookID`),
+  KEY `isbn` (`ISBN`),
+  CONSTRAINT `isbn` FOREIGN KEY (`ISBN`) REFERENCES `ISBNs` (`ISBN`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Books`
+--
+
+LOCK TABLES `Books` WRITE;
+/*!40000 ALTER TABLE `Books` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `BorrowRecords`
+--
+
+DROP TABLE IF EXISTS `BorrowRecords`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `BorrowRecords` (
+  `RecordID` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `BookID` int unsigned DEFAULT NULL,
+  `UserID` int unsigned DEFAULT NULL,
+  `BorrowDate` datetime DEFAULT NULL,
+  `ReturnDate` datetime DEFAULT NULL,
+  `FineAmount` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`RecordID`),
+  KEY `bookid` (`BookID`),
+  KEY `userid` (`UserID`),
+  CONSTRAINT `bookid` FOREIGN KEY (`BookID`) REFERENCES `Books` (`BookID`),
+  CONSTRAINT `userid` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BorrowRecords`
+--
+
+LOCK TABLES `BorrowRecords` WRITE;
+/*!40000 ALTER TABLE `BorrowRecords` DISABLE KEYS */;
+/*!40000 ALTER TABLE `BorrowRecords` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ISBNs`
+--
+
+DROP TABLE IF EXISTS `ISBNs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ISBNs` (
+  `ISBN` varchar(13) NOT NULL,
+  `Title` varchar(150) DEFAULT NULL,
+  `Author` varchar(150) DEFAULT NULL,
+  `Genre` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`ISBN`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ISBNs`
+--
+
+LOCK TABLES `ISBNs` WRITE;
+/*!40000 ALTER TABLE `ISBNs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ISBNs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PaymentRecords`
+--
+
+DROP TABLE IF EXISTS `PaymentRecords`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `PaymentRecords` (
+  `PaymentID` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `UserID` int unsigned NOT NULL,
+  `PaymentAmount` decimal(6,2) DEFAULT NULL,
+  `PaymentDate` datetime DEFAULT NULL,
+  PRIMARY KEY (`PaymentID`,`UserID`),
+  KEY `useridpr` (`UserID`),
+  CONSTRAINT `useridpr` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PaymentRecords`
+--
+
+LOCK TABLES `PaymentRecords` WRITE;
+/*!40000 ALTER TABLE `PaymentRecords` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PaymentRecords` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `Users`
 --
 
@@ -62,4 +171,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-04-27 21:55:52
+-- Dump completed on 2026-04-27 22:35:33
