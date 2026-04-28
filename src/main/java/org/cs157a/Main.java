39b782c4688ec9f9e7b2c73de793a7febd2688d7
Sneha <sneha.nalla@sjsu.edu>
2026-04-27 23:16:26 -0700

    created test jdbc connector, added db.properties

--- src/main/java/org/cs157a/Main.java
+++ src/main/java/org/cs157a/Main.java
@@ -1,8 +1,41 @@
 package org.cs157a;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.io.InputStream;
+import java.util.Properties;
+
 
 public class Main {
     public static void main(String[] args) {
+        Properties props = new Properties();
 
-        //code here
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("db.properties")) {
+
+            if (input == null) {
+                throw new RuntimeException("db.properties not found");
+            }
+
+            props.load(input);
+
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String password = props.getProperty("db.password");
+
+            try (Connection conn = DriverManager.getConnection(url, user, password);
+                 Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery("SHOW TABLES")) {
+                System.out.println("Connected successfully!");
+
+                while (rs.next()) {
+                    System.out.println("Table: " + rs.getString(1));
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
